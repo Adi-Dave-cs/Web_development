44e6876c8bcda8a5532fb31ccc1c3e17e4114271
Aditya Dave <adityardave300@gmail.com>
2024-03-31 18:18:33 +0530

    Misc changes

--- Javascript/script1.js
+++ Javascript/script1.js
@@ -6,3 +6,15 @@ console.log(a === b);
 
 a = undefined
 console.log(a === b);
+
+let arrowfunc = (a,b) => {
+    return a + b;
+}
+
+console.log(arrowfunc)
+console.log(arrowfunc(2,4))
+
+let a1 = 1;
+const b1 = "3";
+
+console.log(parseInt(b1)==a1)
